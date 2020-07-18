7ddc211df0d1911448d449c5906dba013a3ac17d
Sarvesh Kumar Sharma <shsarv@gmail.com>
2020-07-19 00:13:48 +0530

    updated bubble and insertion sort

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,9 +1,11 @@
 # bubble sorting
 def bubble_sort(list2):
-    # swap_test = False
-    for i in range(0, len(list2) - 1):
+    # Traverse through array elements 1 less then size of array
+    n = len(list2)
+    for i in range(0, n - 1):
+        # swap_test = False
         swap_test = False
-        for j in range(0, len(list2) - i - 1):
+        for j in range(0, n - i - 1):
             if list2[j] > list2[j + 1]:
                 list2[j], list2[j + 1] = list2[j + 1], list2[j]  # swapping
             swap_test = True
@@ -15,3 +17,9 @@ def bubble_sort(list2):
 # main
 l = list(map(int, input("enter list items ").split(" ")))
 print(bubble_sort(l))
+
+# output-----
+# enter list items 40 10 39 99 46 15
+# [10, 15, 39, 40, 46, 99]
+#
+# Process finished with exit code 0
--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -1,5 +1,6 @@
 def insertion_sort(list2):
-    for i in range(1, len(list2)):
+    n = len(list2)
+    for i in range(1, n):
         save = list2[i]
         j = i
         while j > 0 and list2[j - 1] > save:
@@ -7,3 +8,12 @@ def insertion_sort(list2):
             j -= 1
         list2[j] = save
     return list2
+
+
+# main
+l = list(map(int, input("enter list items ").split(" ")))
+print(insertion_sort(l))
+
+#  output-----
+# enter list items 100 3 20 900 32 11
+# [3, 11, 20, 32, 100, 900]
