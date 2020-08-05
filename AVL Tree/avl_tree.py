4a840c0af75911046535f01366d07218d23d06c8
Sarvesh Kumar Sharma <shsarv@gmail.com>
2020-08-05 11:08:10 +0530

    updated
    Signed-off-by: Sarvesh Kumar Sharma <shsarv@gmail.com>

--- AVL Tree/avl_tree.py
+++ AVL Tree/avl_tree.py
@@ -1,9 +1,6 @@
 """
 Implementation of an auto-balanced binary tree!
-For doctests run following command:
-python3 -m doctest -v avl_tree.py
-For testing run:
-python avl_tree.py
+Author : Sarvesh Kumar Sharma
 """
 
 import math
